830683f54d027c135f1d3535568822f6dcb152c1
Jimmy Fang <jimmyfang23@gmail.com>
2023-03-01 13:53:18 -0500

    Fixed navbar, moved homepage to appropriate file

--- src/App.js
+++ src/App.js
@@ -1,125 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Homepage from './components/Homepage';
+
+
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
       </Routes>
     </div>
   );
 }
-
 const navStyle = {textDecoration: "none", color: "blue"};
-function Layout(){
-  return(
-    <div className="body">
-      <div className='wrapper_navbar'>
-        <nav className='navbar'>
-          <ul className='navbar_list'>
-            <span>
-              <li className='navbar_item'>
-              <Link style={navStyle} to="/">Homepage</Link>
-              </li>
-            </span>
-            <span>
-              <li className='navbar_item'>
-              <Link style={navStyle} to="/">Homepage</Link>
-              </li>
-            </span>
-            <span>
-              <li className='navbar_item'>
-              <Link style={navStyle} to="/">Homepage</Link>
-              </li>
-            </span>
-            <span>
-              <li className='navbar_item'>
-              <Link style={navStyle} to="/">Homepage</Link>
-              </li>
-            </span>
-          </ul>
-        </nav>
-
-        {/* Slideshow */}
-
-      </div>
-      <div className='wrapper_about'>
-        <div className='section_about'>
-          <div className='row'>
-            <div className='column'>
-              <div id='header' className='row'>
-              <div className='column-placeholder'></div>
-                <div className='title-column'>Random Title</div>
-                <div className='column-placeholder'></div>
-                <div className='column-placeholder'></div>
-              </div>
-              <div id='information' className='row'>
-                <div className='column-placeholder'></div>
-                <div className='image-column'>
-                  <img src></img>
-                </div>
-                <div className='text-column'>
-                  <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
-                </div>
-                <div className='column-placeholder'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='row'>
-            <div className='column'>
-              <div id='header' className='row'>
-              <div className='column-placeholder'></div>
-                <div className='column-placeholder'></div>
-                <div className='title-column'>Random Title</div>
-                <div className='column-placeholder'></div>
-              </div>
-              <div id='information' className='row'>
-                <div className='column-placeholder'></div>
-                <div className='text-column'>
-                  <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
-                </div>
-                <div className='image-column'>
-                <img src></img>
-                </div>
-                <div className='column-placeholder'></div>
-              </div>
-            </div>
-          </div>
-
-          <div className='row'>
-            <div className='column'>
-              <div id='header' className='row'>
-              <div className='column-placeholder'></div>
-                <div className='title-column'>Random Title</div>
-                <div className='column-placeholder'></div>
-                <div className='column-placeholder'></div>
-              </div>
-              <div id='information' className='row'>
-                <div className='column-placeholder'></div>
-                <div className='image-column'>
-                  <img src></img>
-                </div>
-                <div className='text-column'>
-                  <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
-                </div>
-                <div className='column-placeholder'></div>
-              </div>
-            </div>
-          </div>
-
-
-
-        </div>
-      </div>
+function Layout() {
+  return (
+    <div className='wrapper_navbar'>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav className='navbar'>
+        <ul className='navbar_list'>
+          <li className='navbar_item'>
+            <Link style={navStyle} to="/">Home</Link>
+          </li>
+          <li className='navbar_item'>
+            <Link style={navStyle} to="/lodging">Some Link</Link>
+          </li>
+          <li className='navbar_item'>
+            <Link style={navStyle} to="/restaurants">Some Link</Link>
+          </li>
+          <li className='navbar_item'>
+            <Link style={navStyle} to="/attractions">Some Link</Link>
+          </li>
+          <li className='navbar_item'>
+            <Link style={navStyle} to="/nothing-here">Some Link</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
     </div>
   );
 }
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
---
+++ src/components/Homepage.js
@@ -0,0 +1,104 @@
+import './css/Homepage.css'
+
+function Homepage(){
+    return(
+        <div className="body">
+            {/* Slideshow */}
+            <div id='center' className='wrapper'>
+              <div id='h'>
+              <div className='row'>
+                <div className='column'>
+                    <div className='row'>
+                        <div className='column'>dd</div>
+                        <div className='column'>dd</div>
+                        <div className='column'>dd</div>
+                    </div>
+                </div>
+
+
+                <div className='placeholder-column'>d</div>
+                <div className='placeholder-column'>d</div>
+              </div>
+              <div className='row'>d</div>
+              <div className='row'>d</div>
+              <div className='row'>
+                <div className='placeholder-column'>d</div>
+              </div>
+            </div>
+        </div>
+              
+              <div className='wrapper'>
+              <div className='row'>
+                <div className='column'>
+                  <div id='header' className='row'>
+                  <div className='column-placeholder'></div>
+                    <div className='title-column'>Random Title</div>
+                    <div className='column-placeholder'></div>
+                    <div className='column-placeholder'></div>
+                  </div>
+                  <div id='information' className='row'>
+                    <div className='column-placeholder'></div>
+                    <div className='image-column'>
+                      <img src></img>
+                    </div>
+                    <div className='text-column'>
+                      <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+                    </div>
+                    <div className='column-placeholder'></div>
+                  </div>
+                </div>
+              </div>
+    
+              <div className='row'>
+                <div className='column'>
+                  <div id='header' className='row'>
+                  <div className='column-placeholder'></div>
+                    <div className='column-placeholder'></div>
+                    <div className='title-column'>Random Title</div>
+                    <div className='column-placeholder'></div>
+                  </div>
+                  <div id='information' className='row'>
+                    <div className='column-placeholder'></div>
+                    <div className='text-column'>
+                      <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+                    </div>
+                    <div className='image-column'>
+                    <img src></img>
+                    </div>
+                    <div className='column-placeholder'></div>
+                  </div>
+                </div>
+              </div>
+    
+              <div className='row'>
+                <div className='column'>
+                  <div id='header' className='row'>
+                  <div className='column-placeholder'></div>
+                    <div className='title-column'>Random Title</div>
+                    <div className='column-placeholder'></div>
+                    <div className='column-placeholder'></div>
+                  </div>
+                  <div id='information' className='row'>
+                    <div className='column-placeholder'></div>
+                    <div className='image-column'>
+                      <img src></img>
+                    </div>
+                    <div className='text-column'>
+                      <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+                    </div>
+                    <div className='column-placeholder'></div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      );
+}
+
+export default Homepage;
