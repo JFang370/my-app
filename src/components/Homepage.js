077a94beddb946955f9b0d979ad1de9c6b13ee63
Jimmy Fang <jimmyfang23@gmail.com>
2023-03-02 23:53:44 -0500

    Adjusted layout of page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,101 +2,24 @@ import './css/Homepage.css'
 
 function Homepage(){
     return(
-        <div className="body">
-            {/* Slideshow */}
-            <div id='center' className='wrapper'>
-              <div id='h'>
-              <div className='row'>
-                <div className='column'>
-                    <div className='row'>
-                        <div className='column'>dd</div>
-                        <div className='column'>dd</div>
-                        <div className='column'>dd</div>
-                    </div>
+        <div className="container">
+            <div className="main">
+              <div className="main-left">
+              <div className="left-placeholder"></div>
+                <div className="left-content">
                 </div>
-
-
-                <div className='placeholder-column'>d</div>
-                <div className='placeholder-column'>d</div>
+                <div className="left-placeholder"></div>
+                <div className="background"></div>
               </div>
-              <div className='row'>d</div>
-              <div className='row'>d</div>
-              <div className='row'>
-                <div className='placeholder-column'>d</div>
+              <div className="main-right">
+                <div className="right-content"></div>
+                <div className="right-content"></div>
+                <div className="right-content"></div>
               </div>
             </div>
-        </div>
-              
-              <div className='wrapper'>
-              <div className='row'>
-                <div className='column'>
-                  <div id='header' className='row'>
-                  <div className='column-placeholder'></div>
-                    <div className='title-column'>Random Title</div>
-                    <div className='column-placeholder'></div>
-                    <div className='column-placeholder'></div>
-                  </div>
-                  <div id='information' className='row'>
-                    <div className='column-placeholder'></div>
-                    <div className='image-column'>
-                      <img src></img>
-                    </div>
-                    <div className='text-column'>
-                      <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-                    </div>
-                    <div className='column-placeholder'></div>
-                  </div>
-                </div>
-              </div>
-    
-              <div className='row'>
-                <div className='column'>
-                  <div id='header' className='row'>
-                  <div className='column-placeholder'></div>
-                    <div className='column-placeholder'></div>
-                    <div className='title-column'>Random Title</div>
-                    <div className='column-placeholder'></div>
-                  </div>
-                  <div id='information' className='row'>
-                    <div className='column-placeholder'></div>
-                    <div className='text-column'>
-                      <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-                    </div>
-                    <div className='image-column'>
-                    <img src></img>
-                    </div>
-                    <div className='column-placeholder'></div>
-                  </div>
-                </div>
-              </div>
-    
-              <div className='row'>
-                <div className='column'>
-                  <div id='header' className='row'>
-                  <div className='column-placeholder'></div>
-                    <div className='title-column'>Random Title</div>
-                    <div className='column-placeholder'></div>
-                    <div className='column-placeholder'></div>
-                  </div>
-                  <div id='information' className='row'>
-                    <div className='column-placeholder'></div>
-                    <div className='image-column'>
-                      <img src></img>
-                    </div>
-                    <div className='text-column'>
-                      <div className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-                    </div>
-                    <div className='column-placeholder'></div>
-                  </div>
-                </div>
-              </div>
-          </div>
+            <div className="footer">
+              d
+            </div>
         </div>
       );
 }
