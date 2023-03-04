51359b30fa8e61786240f21be6295f0b4457d53f
Jimmy Fang <jimmyfang23@gmail.com>
2023-03-04 00:37:07 -0500

    basic layout created

--- src/components/Food.js
+++ src/components/Food.js
@@ -2,7 +2,14 @@ import './css/Food.css';
 
 function Food(){
     return(
-        <div></div>
+        <div id='food-container' className='container'>
+            <div className='food-content1'>Food 1</div>
+            <div className='food-content2'>Food 2</div>
+            <div className='food-content3'>Food 3</div>
+            <div className='food-content4'>Food 4</div>
+            <div className='food-content5'>Food 5</div>
+        </div>
+        
     )
 }
 
