7aecc4140ff4a8e0616806444fbeda2596831b02
Jimmy Fang <jimmyfang23@gmail.com>
2023-03-05 17:26:03 -0500

    necessary files created

---
+++ src/components/Transportation.js
@@ -0,0 +1,22 @@
+import './css/Transportation.css';
+
+function Transportation(){
+    return(
+        <div id='transportation' className='container'>
+            <div id='map'>
+                
+            </div>
+            <div id='option-1' className='transportation-content'>
+
+            </div>
+            <div id='option-1' className='transportation-content'>
+                
+            </div>
+            <div id='option-1' className='transportation-content'>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Transportation;
